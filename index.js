c584c5d3d1f16205aab9edcd483d4d7a7895a0aa
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-24 15:36:04 +0500

    done

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-async function waitkr(){
- return new Promise((resolve) =>{
- let randomtime = Math.random()*6000 + 1000;
- setTimeout(()=>{
-    resolve();
- }, randomtime);
- });
+async function waitkr() {
+  return new Promise((resolve) => {
+    let randomtime = Math.random() * 6000 + 1000;
+    setTimeout(() => {
+      resolve();
+    }, randomtime);
+  });
 }
-async function starthacking(){
-    document.getElementById("container").innerHTML +=
-      "<p>intializing hacking<span class=`blink.dots`>...</span></p>";
-    await waitkr();
- document.getElementById("container").innerHTML +=
-   "<p>reading your files<span class=`blink.dots`>...</span></p>";
- await waitkr();
- document.getElementById("container").innerHTML +=
-   "<p>password detected<span class=`blink.dots`>...</span></p>";
- await waitkr();
- document.getElementById("container").innerHTML +=
-   "<p>sending all fils to server<span class=`blink.dots`>...</span></p>";
- await waitkr();
- document.getElementById("container").innerHTML +=
-   "<p>reading clean up<span class=`blink.dots`>...</span></p>";
- await waitkr();
- 
+async function starthacking() {
+  document.getElementById("container").innerHTML +=
+    "<p>intializing hacking<span class='blink-dots'>...</span></p>";
+  await waitkr();
+  document.getElementById("container").innerHTML +=
+    "<p>reading your files<span class='blink-dots'>...</span></p>";
+  await waitkr();
+  document.getElementById("container").innerHTML +=
+    "<p>password detected<span class='blink-dots'>...</span></p>";
+  await waitkr();
+  document.getElementById("container").innerHTML +=
+    "<p>sending all fils to server<span class='blink-dots'>...</span></p>";
+  await waitkr();
+  document.getElementById("container").innerHTML +=
+    "<p> clean up<span class='blink-dots'>...</span></p>";
+  await waitkr();
 }
+starthacking();
