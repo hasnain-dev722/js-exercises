2254d7001e3ce5b71113ef03510dc1372c245399
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-25 15:03:43 +0500

    try catch

--- index.js
+++ index.js
@@ -28,5 +28,17 @@
 // 
 let a = prompt ("enter a number")
 let b = prompt ("enter second number")
+if(isNaN(a) && isNaN(b)){
+  throw SyntaxError("sorry this is not allowed")
+}
 let sum = parseInt(a)+parseInt(b)
-console.log("the sum is", sum)
+try{
+console.log("the sum is", sum*x)
+} catch (error) {
+ console.log('error agya bhai');
+ 
+}
+finally{
+  console.log('files closed ha');
+  
+}
