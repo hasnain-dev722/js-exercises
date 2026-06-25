66ab042499aa9bdde70d75af65fe467233644f35
hasnain-dev722 <kharalhasnain636@gmail.com>
2026-06-25 14:34:33 +0500

    try catch

--- index.js
+++ index.js
@@ -25,20 +25,6 @@
 // }
 // starthacking();
 
-let users = [
-  {items: 4,totalprice: 6000,coupon: "yes" }
-
-]
-let newarray = users.map((anything)=>{
-  if(anything.totalprice<=6000){
-    console.log(`delivery free to you`)
-  }else{
-    console.log(`delivery charges rs 300`)
-  }
-  if(anything.coupon=== "yes",anything.items<=4){
-    console.log(`you apply coupon get 50% discount`)
-       anything.totalprice = 3000;
-  }
-  return anything;
-});
-console.log(newarray)
+// 
+let a = (`enter a number`)
+let b = (`enter second number`)
